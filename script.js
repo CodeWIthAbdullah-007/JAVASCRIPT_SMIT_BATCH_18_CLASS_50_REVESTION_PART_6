b451a676742cb1f937b78408e55a5381aa52d159
CodeWithAbdullah-007 <mabdullahanwar85@gmail.com>
2026-04-06 23:03:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// hello();
+
+// function hello() {
+//     console.log("hello World"); 
+// }
+
+// hello();
+
+// var hello = () => {
+//     console.log("Hello World");
+// };
+
+console.log(user);
+var user = 'Majid'
